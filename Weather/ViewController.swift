ad0b44135bbb084cc1a44c34278f3a518461638a
Cesar Tesen <clutesen@gmail.com>
2020-03-26 17:56:16 -0700

    JSON parsing finally working, data model is filled and pushed to view correctly

--- Weather/ViewController.swift
+++ Weather/ViewController.swift
@@ -8,34 +8,36 @@
 
 import UIKit
 
-var weather = Weather()
+var parsedWeather = Weather()
 
 class ViewController: UIViewController {
-
-    @IBOutlet weak var Temperature: UILabel!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         loadWeather()
-        Temperature.text = "\(weather.temp)°"
+        Temperature.text = "\(parsedWeather.main.temp)°"
     }
-
-
+    
+    @IBAction func updateWeather(_ sender: Any) {
+        loadWeather()
+        Temperature.text = "\(parsedWeather.main.temp)°"
+    }
+    @IBOutlet weak var Temperature: UILabel!
 }
 
 let session: URLSession = .shared
 private func loadWeather()
 {
-  let url = URL(string: "http://api.openweathermap.org/data/2.5/weather?lat=33.952763&lon=118.4405604&appid=45e00b3433c2ad9f4a0575e78c775d79")!
-  let task = session.dataTask(with: url) { (data, response, error) in
-      guard let data = data else { return }
-      do {
-        weather = try parse(data)
-      }
-      catch {
-          print("JSONParsing Error: \(error)")
-      }
-  }
-  task.resume()
+    let url = URL(string: "http://api.openweathermap.org/data/2.5/weather?lat=33.952763&lon=118.4405604&appid=45e00b3433c2ad9f4a0575e78c775d79")!
+    let task = session.dataTask(with: url) { (data, response, error) in
+        guard let data = data else { return }
+        do {
+            parsedWeather = try JSONDecoder().decode(Weather.self, from: data)
+            print(parsedWeather.main.temp)
+        }
+        catch {
+            print("JSONParsing Error: \(error)")
+        }
+    }
+    task.resume()
 }
--- Weather/Weather.swift
+++ Weather/Weather.swift
@@ -8,25 +8,122 @@
 
 import Foundation
 
-struct Weather
-{
-    let temp: Double
-//    let feels_like: Double
-//
-//    let pressure: Int
-//    let humidity: Int
-//
-//    let sea_level: Int
-//    let grnd_level: Int
+// MARK: - Weather
+struct Weather: Codable {
+    let coord: Coord
+    let weather: [WeatherElement]
+    let base: String
+    let main: Main
+    let wind: Wind
+    let clouds: Clouds
+    let dt: Int
+    let sys: Sys
+    let timezone, id: Int
+    let name: String
+    let cod: Int
+    
     init() {
-        temp = 0
+        coord = Coord()
+        weather = [WeatherElement()]
+        base = "?"
+        main = Main()
+        wind = Wind()
+        clouds = Clouds()
+        dt = 0
+        sys = Sys()
+        timezone = 0
+        id = 0
+        name = "?"
+        cod = 0
     }
 }
 
-extension Weather: JSONDecodable
-{
-  init(_ decoder: JSONDecoder) throws
-  {
-    self.temp = try decoder.value(forKey: "main")
-  }
+// MARK: - Clouds
+struct Clouds: Codable {
+    let all: Int
+    
+    init() {
+        all = 0
+    }
 }
+
+// MARK: - Coord
+struct Coord: Codable {
+    let lon, lat: Double
+    
+    init() {
+        lon = 0
+        lat = 0
+    }
+}
+
+// MARK: - Main
+struct Main: Codable {
+    let temp: Double
+    let feelsLike, tempMin, tempMax: Double
+    let pressure, humidity, seaLevel, grndLevel: Int
+
+    enum CodingKeys: String, CodingKey {
+        case temp
+        case feelsLike = "feels_like"
+        case tempMin = "temp_min"
+        case tempMax = "temp_max"
+        case pressure, humidity
+        case seaLevel = "sea_level"
+        case grndLevel = "grnd_level"
+    }
+    
+    init() {
+        temp = 0
+        feelsLike = 0
+        tempMin = 0
+        tempMax = 0
+        pressure = 0
+        humidity = 0
+        seaLevel = 0
+        grndLevel = 0
+    }
+}
+
+// MARK: - Sys
+struct Sys: Codable {
+    let country: String
+    let sunrise, sunset: Int
+    
+    init() {
+        country = "?"
+        sunrise = 0
+        sunset = 0
+    }
+}
+
+// MARK: - WeatherElement
+struct WeatherElement: Codable {
+    let id: Int
+    let main, weatherDescription, icon: String
+
+    enum CodingKeys: String, CodingKey {
+        case id, main
+        case weatherDescription = "description"
+        case icon
+    }
+    
+    init() {
+        id = 0
+        main = "?"
+        weatherDescription = "?"
+        icon = "?"
+    }
+}
+
+// MARK: - Wind
+struct Wind: Codable {
+    let speed: Double
+    let deg: Int
+    
+    init() {
+        speed = 0
+        deg = 0
+    }
+}
+
